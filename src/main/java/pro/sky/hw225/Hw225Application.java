66a5fc9c94b14d0315b2b8361435c29cf3ae57d5
amba404 <amba.avg@gmail.com>
2024-10-27 13:45:20 +0900

    Initial SpringBoot

---
+++ src/main/java/pro/sky/hw225/Hw225Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hw225;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw225Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw225Application.class, args);
+	}
+
+}
