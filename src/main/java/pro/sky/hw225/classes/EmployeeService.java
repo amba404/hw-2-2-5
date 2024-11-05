15446b7046c48baeb645bf971f39af54ef37c477
amba404 <amba.avg@gmail.com>
2024-11-05 23:48:17 +0900

    HW 2-2-7 is done here

--- src/main/java/pro/sky/hw225/classes/EmployeeService.java
+++ src/main/java/pro/sky/hw225/classes/EmployeeService.java
@@ -1,20 +1,18 @@
 package pro.sky.hw225.classes;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import pro.sky.hw225.exceptions.EmployeeAlreadyAddedException;
 import pro.sky.hw225.exceptions.EmployeeNotFoundException;
 import pro.sky.hw225.exceptions.EmployeeStorageIsFullException;
 import pro.sky.hw225.interfaces.EmployeeServiceInterface;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService implements EmployeeServiceInterface {
     final private int maxEmployeeCount = 10;
-    private final List<Employee> employees = new ArrayList<>();
+    private final Map<String, Employee> employees = new HashMap<>();
 
     @Override
     public Employee add(String firstName, String lastName) {
@@ -22,10 +20,10 @@ public class EmployeeService implements EmployeeServiceInterface {
             throw new EmployeeStorageIsFullException("Все вакансии заполнены");
         }
 
-        Employee newEmployee = new Employee(firstName, lastName);
-
-        if (!employees.contains(newEmployee)) {
-            employees.add(newEmployee);
+        EmployeeHelper empHlp = new EmployeeHelper(firstName, lastName);
+        Employee newEmployee = new Employee(empHlp.firstName, empHlp.lastName);
+        if (!employees.containsKey(empHlp.mapKey)) {
+            employees.put(empHlp.mapKey, newEmployee);
             return newEmployee;
         } else {
             throw new EmployeeAlreadyAddedException("Сотрудник уже добавлен: " + newEmployee);
@@ -34,9 +32,10 @@ public class EmployeeService implements EmployeeServiceInterface {
 
     @Override
     public Employee remove(String firstName, String lastName) {
-        Employee delEmployee = new Employee(firstName, lastName);
-        if (employees.contains(delEmployee)) {
-            employees.remove(delEmployee);
+        EmployeeHelper empHlp = new EmployeeHelper(firstName, lastName);
+        Employee delEmployee = new Employee(empHlp.firstName, empHlp.lastName);
+        if (employees.containsKey(empHlp.mapKey)) {
+            employees.remove(empHlp.mapKey);
             return delEmployee;
         } else {
             throw new EmployeeNotFoundException("Сотрудник не найден: " + delEmployee);
@@ -45,8 +44,9 @@ public class EmployeeService implements EmployeeServiceInterface {
 
     @Override
     public Employee find(String firstName, String lastName) {
-        Employee findEmployee = new Employee(firstName, lastName);
-        Employee result = find(findEmployee);
+        EmployeeHelper empHlp = new EmployeeHelper(firstName, lastName);
+        Employee findEmployee = new Employee(empHlp.firstName, empHlp.lastName);
+        Employee result = employees.get(empHlp.mapKey);
         if (result != null) {
             return result;
         }
@@ -55,11 +55,27 @@ public class EmployeeService implements EmployeeServiceInterface {
 
     @Override
     public Collection<Employee> getList() {
-        return Collections.unmodifiableList(employees);
+        return List.copyOf(employees.values());
     }
 
-    private Employee find(Employee findEmployee) {
-        int idx = employees.indexOf(findEmployee);
-        return idx < 0 ? null : employees.get(idx);
+    private static class EmployeeHelper {
+        final String firstName;
+        final String lastName;
+        final String mapKey;
+
+        public EmployeeHelper(@NotNull String firstName, @NotNull String lastName) {
+            this.firstName = getCleanName(firstName);
+            this.lastName = getCleanName(lastName);
+            this.mapKey = this.lastName + this.firstName;
+        }
+
+        private static @NotNull String getCleanName(@NotNull String name) {
+            name = name.trim().toLowerCase(Locale.ROOT);
+            if (!name.isEmpty()) {
+                name = name.substring(0, 1).toUpperCase(Locale.ROOT) + name.substring(1);
+            }
+            return name;
+        }
+
     }
 }
